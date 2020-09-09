2d1f8b9838903cb41bf8ce4372d6dbb29bb5e00a
yyavor0705 <yyavor0507@gmail.com>
2020-09-09 12:48:11 +0300

    Removed App.css. Added Open Sans fonts. Changed title.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">I'm a burger project first page !
+      <div>I'm a burger project first page !
       </div>
     );
   }
