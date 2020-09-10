47f0555180b7e3546de2fc6558ba9315eec18768
yyavor0705 <yyavor0507@gmail.com>
2020-09-10 09:57:44 +0300

    Added BurgerConstructor container and usage of 'Open Sans' font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout'
+import BurgerConstructor from './components/containers/BurgerConstructor/BurgerConstructor';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          I'm a burger project first page !
+          <BurgerConstructor />
         </Layout>
       </div>
     );
---
+++ src/components/containers/BurgerConstructor/BurgerConstructor.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+import Aux from '../../../hoc/Auxiliary';
+
+
+class BurgerConstructor extends Component {
+    render () {
+        return (
+        <Aux>
+            <div>Burger</div>
+            <div>Burger Controls</div>
+        </Aux>
+        )
+    }
+}
+
+export default BurgerConstructor
